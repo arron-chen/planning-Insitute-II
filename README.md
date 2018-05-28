a24726560ca9867c77d6953c11c1da6d69deed2c
chentao <1161735119@qq.com>
2018-05-28 18:17:20 +0800

    clear

--- README.md
+++
@@ -1,4 +0,0 @@
-# vue-admin
-Vue+ElementUI实现选卡式后台Admin
-
-预览地址：http://othcosaz6.bkt.clouddn.com
--- src/filter/date/index.js
+++
@@ -1,7 +0,0 @@
-import Vue from 'vue'
-import { dateFormat } from '@/tools/date/format.js'
-
-
-Vue.filter('date', v => {
-  return dateFormat(new Date(v), 'YYYY-MM-DD HH:mm:ss')
-})
