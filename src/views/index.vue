b814fd989945a36b417beb55219d181f229cefd5
xiexiaoyi <xiaoyi_xie@yeah.net>
2018-05-30 16:21:54 +0800

    菜单

--- src/views/index.vue
+++ src/views/index.vue
@@ -21,7 +21,7 @@
     <div class="nav">
       <div class="collapse-btn" @click="taggleNav()"></div>
       <el-menu class="el-menu-vertical-demo"  unique-opened  :collapse="isCollapse">
-        <el-menu-item v-for="(item,index) of items" :index="item.name" :key="index" @click="addTab(item)">
+        <el-menu-item v-for="(item,index) of secondMenu" :index="item.moduleId" :key="index" @click="addTab(item)">
           <i class="el-icon-menu"></i>
           <span slot="title">{{item.name}}</span>
         </el-menu-item>
@@ -31,7 +31,7 @@
       <el-tabs v-model="activeName" type="card"closable @tab-remove="removeTab">
         <el-tab-pane
           :key="item.name"
-          v-for="(item, index) in menuList"
+          v-for="(item, index) in tabsList"
           :label="item.title"
           :name="item.name"
         >
@@ -47,20 +47,71 @@
   export default {
     data(){
       return {
-        menuList:[],
-        items:[{
-          'name':'m1'
-        },{
-          'name':'m1'
-        },{
-          'name':'m1'
-        },{
-          'name':'m1'
-        }],
+        tabsList:[],
+        // items:[],
+        activeMenu:'20ddfe2c-096d-492e-ae02-140e1a77aaf8',
+        items1:[
+          {"moduleId": null,
+            "name": null,
+            "pageUrl": null,
+            "imageUrl": null,
+            "children": [
+          {"moduleId": "20ddfe2c-096d-492e-ae02-140e1a77aaf8",
+          "name": "业务设置",
+          "pageUrl": null,
+          "imageUrl": "/mainWeb/images/mobile/系统设置/symset.png",
+          "children": [{
+            "moduleId": "1eda0594-4b90-4363-80ea-137b9a8199b0",
+            "name": "业务详情设置",
+            "pageUrl": "/pubWeb/system/getMatterDeclareSetting",
+            "imageUrl": null,
+            "children": null,
+            "parentId": "20ddfe2c-096d-492e-ae02-140e1a77aaf8",
+            "sort": 0,
+            "position": 100
+          }, {
+            "moduleId": "b645754d-449d-4ca4-8125-82e82534c3af",
+            "name": "作业调度管理",
+            "pageUrl": "/mainWeb/system/showJobSchedule",
+            "imageUrl": "/mainWeb/images/mobile/系统设置/17.png",
+            "children": null,
+            "parentId": "20ddfe2c-096d-492e-ae02-140e1a77aaf8",
+            "sort": 0,
+            "position": 100
+          }, {
+            "moduleId": "8da5f948-681c-43d9-8264-25156ecc3471",
+            "name": "子系统",
+            "pageUrl": "/mainWeb/system/subSystem",
+            "imageUrl": null,
+            "children": null,
+            "parentId": "20ddfe2c-096d-492e-ae02-140e1a77aaf8",
+            "sort": 0,
+            "position": 100
+          }]
+          }]
+          }
+          ],
         activeName:'1',
         tabIndex: 0,
         userName:'周冬雨',
         isCollapse: false,
+      }
+    },
+    computed : {
+      menuList(){
+        // console.log(this.items1);
+        return this.items1[0].children;
+      },
+      firstMenu(){
+        return this.menuList;
+      },
+      secondMenu(){
+        if(!this.activeMenu){return []}
+        for(var i=0;i<this.menuList.length;i++){
+          if(this.activeMenu == this.menuList[i].moduleId){
+            return this.menuList[i].children;
+          }
+        }
       }
     },
     methods : {
@@ -73,7 +124,7 @@
       },
       addTab(item) {
           let newTabName = ++this.tabIndex + '';
-          this.menuList.push({
+          this.tabsList.push({
             title: item.name,
             name: newTabName,
             content: 'New Tab content'
@@ -81,7 +132,7 @@
           this.activeName = item.name;
       },
       removeTab(targetName) {
-        let tabs = this.menuList;
+        let tabs = this.tabsList;
         let activeName = this.activeName;
         if (activeName === targetName) {
           tabs.forEach((tab, index) => {
